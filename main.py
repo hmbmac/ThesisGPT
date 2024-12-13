2163b0f5e3c749ec71a4d596fcf64d7c0eb359d1
Hash <hashem.benamar@maghrebnet.com>
2024-12-13 16:34:26 -0500

    Heroku Deployment Error Fix Attempt #2

--- main.py
+++ main.py
@@ -238,4 +238,4 @@ async def index():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 8080))
-    uvicorn.run("main:app", host='0.0.0.0', port=port, reload=True)
+    uvicorn.run("main:app", host='0.0.0.0', port=port)
